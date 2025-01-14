5d2d9a5342b4021c9f86339270fcc85e421f3a89
websseu <webstoryboy@naver.com>
2025-01-14 12:46:46 +0900

    12-create-cart-page

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -17,13 +17,14 @@ const initialState: Cart = {
 interface CartState {
   cart: Cart
   addItem: (item: OrderItem, quantity: number) => Promise<string>
+  updateItem: (item: OrderItem, quantity: number) => Promise<void>
+  removeItem: (item: OrderItem) => void
 }
 
 const useCartStore = create(
   persist<CartState>(
     (set, get) => ({
       cart: initialState,
-
       addItem: async (item: OrderItem, quantity: number) => {
         const { items } = get().cart
         const existItem = items.find(
@@ -69,6 +70,50 @@ const useCartStore = create(
             x.color === item.color &&
             x.size === item.size
         )?.clientId!
+      },
+      updateItem: async (item: OrderItem, quantity: number) => {
+        const { items } = get().cart
+        const exist = items.find(
+          (x) =>
+            x.product === item.product &&
+            x.color === item.color &&
+            x.size === item.size
+        )
+        if (!exist) return
+        const updatedCartItems = items.map((x) =>
+          x.product === item.product &&
+          x.color === item.color &&
+          x.size === item.size
+            ? { ...exist, quantity: quantity }
+            : x
+        )
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calcDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
+        })
+      },
+      removeItem: async (item: OrderItem) => {
+        const { items } = get().cart
+        const updatedCartItems = items.filter(
+          (x) =>
+            x.product !== item.product ||
+            x.color !== item.color ||
+            x.size !== item.size
+        )
+        set({
+          cart: {
+            ...get().cart,
+            items: updatedCartItems,
+            ...(await calcDeliveryDateAndPrice({
+              items: updatedCartItems,
+            })),
+          },
+        })
       },
       init: () => set({ cart: initialState }),
     }),
