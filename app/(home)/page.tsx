b39956a7de91933ae5d97849ff8dee1681f79a00
websseu <webstoryboy@naver.com>
2025-01-14 12:31:47 +0900

    08-create-best-selling-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,6 +12,8 @@ import { toSlug } from '@/lib/utils'
 
 export default async function HomePage() {
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
+
   const categories = (await getAllCategories()).slice(0, 4)
   const newArrivals = await getProductsForCard({
     tag: 'new-arrival',
@@ -72,6 +74,15 @@ export default async function HomePage() {
         <Card className='w-full rounded-none'>
           <CardContent className='p-4 items-center gap-3'>
             <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title='Best Selling Products'
+              products={bestSellingProducts}
+              hideDetails
+            />
           </CardContent>
         </Card>
       </div>
