984d6f2ab26012ee716b101f91555cc9bc09e63e
websseu <webstoryboy@naver.com>
2025-01-14 14:54:01 +0900

    15-register-user

--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ import {
   ProductInputSchema,
   UserInputSchema,
   UserSignInSchema,
+  UserSignUpSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
@@ -29,3 +30,5 @@ export type Cart = z.infer<typeof CartSchema>
 
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
+
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
